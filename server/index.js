5b2eddd34360d861cc87e3d8e8069706a223f81a
Yashsingh045 <astomar6396@gmail.com>
2025-12-31 22:28:04 +0530

    Add CORS and JSON middleware

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5001;
 
+app.use(cors());
+app.use(express.json());
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
