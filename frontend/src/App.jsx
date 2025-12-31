3e4b6032b51438e2d6660fe37000eec9c085538f
Yashsingh045 <astomar6396@gmail.com>
2025-12-31 22:35:13 +0530

    Integrate Navbar and fix CSS compatibility

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="app-container">
+      <Navbar />
       <h1>Library Manager</h1>
     </div>
   )
