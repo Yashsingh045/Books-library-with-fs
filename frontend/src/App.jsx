47cbe4c230c7c2cab77c473a5f0b636c319f448d
Yashsingh045 <astomar6396@gmail.com>
2025-12-31 22:38:28 +0530

    Setup application routing with React Router and integrate BookList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import BookList from './components/BookList'
 
 function App() {
   return (
-    <div className="app-container">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<BookList />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
