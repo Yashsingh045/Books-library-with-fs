455ffe96dbb1fd8c6b7c9013d3825491fa8e4f87
Yashsingh045 <astomar6396@gmail.com>
2025-12-31 22:32:38 +0530

    Create basic Navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar glass-card">
+            <div className="nav-logo">LibFlow</div>
+            <div className="nav-links">
+                <button className="btn btn-primary">Add Book</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
